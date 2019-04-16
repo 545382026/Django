474dceba33ba32a77092f1422f1fe587325a78dc
yang <545382026@qq.com>
2019-04-16 21:57:56 +0800

    应用注释

--- demo1/booktest/apps.py
+++ demo1/booktest/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class BooktestConfig(AppConfig):
     name = 'booktest'
+
+
+"""
+ 应用配置文件
+"""
--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+"""
+ django MVT M
+ ORM 对象O
+ 需要定义实体类
+"""
